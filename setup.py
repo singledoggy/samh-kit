527250fde26daf62d0a673c379f5feb6ac3a7f88
Sam Han <fadedflowers@foxmail.com>
2023-08-22 19:14:47 +0800

    feat: init

---
+++ setup.py
@@ -0,0 +1,14 @@
+# This file is if we want to be able to pip install it & upload the package to pypi
+
+from setuptools import setup
+
+
+setup(name='samh-kit',
+      version='0.0.1',
+      description='example python package',
+      author='Yuyang Han',
+      author_email='',
+      url='',
+      packages=['samh_kit'],
+      license='MIT',
+      )
